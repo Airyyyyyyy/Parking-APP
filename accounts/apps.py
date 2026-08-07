62fe4a768f28451b4c79d4eea14d56b45b02b941
Maleeq Lawal <lawalmaleeq44@gmail.com>
2026-08-07 11:36:58 +0100

    Disconnect update_last_login signal (djongo update_fields crash on login)

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # djongo mishandles `save(update_fields=[...])`, which Django's built-in
+        # `update_last_login` receiver uses on every login. Disconnect it so
+        # logging in doesn't crash. We only lose the `last_login` timestamp.
+        from django.contrib.auth import user_logged_in
+        from django.contrib.auth.models import update_last_login
+        user_logged_in.disconnect(update_last_login)
